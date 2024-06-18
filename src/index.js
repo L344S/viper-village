833a15753f5eaa4f8f79dbe7e7c8177971478a88
L344S <145486211+L344S@users.noreply.github.com>
2024-06-18 10:40:12 +0200

    UP: Implement collision in index.js - switch block color to invisible and adjust player hitbox dimensions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Constants
 const GAME_WIDTH = 1024;
 const GAME_HEIGHT = 600;
-const PLAYER_SPEED = 800; // 160 usually but it's too slow for testing
+const PLAYER_SPEED = 160; // 160 usually but it's too slow for testing
 const FRAMERATE = 9;
 
 // TEMPORARY: Set the collision coordinates
@@ -344,8 +344,8 @@ setCollision.add(`864,1632`);
 setCollision.add(`912,1632`);
 setCollision.add(`1248,1632`);
 setCollision.add(`1296,1632`);
-setCollision.add(`1344,1632`);
-setCollision.add(`1392,1632`);
+setCollision.add(`1345,1632`);
+setCollision.add(`1392,1585`);
 setCollision.add(`1488,1632`);
 setCollision.add(`1920,1632`);
 setCollision.add(`2544,1632`);
@@ -498,6 +498,7 @@ const game = new Phaser.Game(config);
 let player;
 let cursors;
 let wasd;
+// let graphics; testing temporary hitbox
 
 function preload() {
   // Load the map and all the player assets
@@ -531,8 +532,10 @@ function create() {
   player = this.physics.add.sprite(470, 380, "player").setScale(3);
   // fix the player's hitbox cuz it's too big with the collision
   player.body
-    .setSize(player.width - 5, player.height - 40, false)
+    .setSize(player.width - 10, player.height - 40, false)
     .setOffset(0, 10);
+
+  //graphics = this.add.graphics(); // testing the border and hitbox
 
   // Call the function to create the player animations for each direction
   createPlayerAnimation("down", this);
@@ -543,14 +546,14 @@ function create() {
   setCollision.forEach((coords) => {
     // going through the set of collision coordinates
     const [x, y] = coords.split(",").map(Number);
-    let redBlock = this.add
-      .rectangle(x, y, 48, 48, 0xff0000)
-      .setOrigin(21, 9.8)
+    let colBlock = this.add
+      .rectangle(x, y, 43, 43, 0x000000, 0)
+      .setOrigin(23.6, 11.2)
       .setScale(1);
 
-    this.physics.add.existing(redBlock);
-    redBlock.body.immovable = true;
-    this.physics.add.collider(player, redBlock); // set the collision between the player and walls
+    this.physics.add.existing(colBlock);
+    colBlock.body.immovable = true;
+    this.physics.add.collider(player, colBlock); // set the collision between the player and walls
   });
   // Set the variables to handle the keyboard inputs (arrow keys and WASD keys)
   cursors = this.input.keyboard.createCursorKeys();
@@ -564,6 +567,21 @@ function create() {
   this.cameras.main.startFollow(player);
   this.cameras.main.setBounds(-800, -800, 5000, 3500);
 }
+
+/*
+function drawBorder() {
+  // test function to draw the border of the player's hitbox to adjust it (only for testing)
+  graphics.clear();
+  graphics.lineStyle(2, 0x00ff00, 1);
+
+  graphics.strokeRect(
+    player.x - (player.width - 10) / 2,
+    player.y - (player.height - 40) / 2,
+    player.width - 10,
+    player.height - 40
+  );
+}
+*/
 
 // Create the player animations for each direction
 function createPlayerAnimation(direction, scene) {
@@ -581,6 +599,7 @@ function createPlayerAnimation(direction, scene) {
 // Update the player's movement based on the keyboard inputs
 function update() {
   player.setVelocity(0);
+  // drawBorder();
   if (cursors.left.isDown || wasd.left.isDown) {
     player.setVelocityX(-PLAYER_SPEED);
     player.anims.play("left", true);
