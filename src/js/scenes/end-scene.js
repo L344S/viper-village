5be4c8a111b2fb9d05f0d910d86e0bee6c96fa24
L344S <145486211+L344S@users.noreply.github.com>
2024-06-26 21:09:37 +0200

    UP: Update asset paths for end scene images

--- src/js/scenes/end-scene.js
+++ src/js/scenes/end-scene.js
@@ -20,10 +20,22 @@ export default class EndScene extends Phaser.Scene {
 
   preload() {
     // Load all assets for the end scene
-    this.load.image("endBackground", "../../assets/end-background.jpg");
-    this.load.image("restartButton", "../../assets/restart-button.png");
-    this.load.image("webpageButton", "../../assets/webpage-button.png");
-    this.load.image("githubButton", "../../assets/github-button.png");
+    this.load.image(
+      "endBackground",
+      "../../assets/visual/scenes/end-background.jpg"
+    );
+    this.load.image(
+      "restartButton",
+      "../../assets/visual/buttons/restart-button.png"
+    );
+    this.load.image(
+      "webpageButton",
+      "../../assets/visual/buttons/webpage-button.png"
+    );
+    this.load.image(
+      "githubButton",
+      "../../assets/visual/buttons/github-button.png"
+    );
   }
 
   /**
