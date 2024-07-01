ec139466a251f5c9433f05fef0a32df89ac1fb6c
yazgahar <battianas@hotmail.com>
2024-07-01 14:19:17 +0200

    function to decrypt file added

--- AES/decrypt.py
+++ AES/decrypt.py
@@ -1,3 +1,5 @@
+from cryptography.fernet import Fernet, InvalidToken
+import os
 
 
 def load_key(key_path='encryption_key.key'):
@@ -11,3 +13,23 @@ def load_key(key_path='encryption_key.key'):
     except Exception as e:
         print(f"Error loading key: {e}")
         return None
+
+
+def decrypt_file(encrypted_file_path, cipher_suite):
+    try:
+        with open(encrypted_file_path, 'rb') as f:
+            encrypted_data = f.read()
+        print(f"Read encrypted data from {encrypted_file_path}")
+
+        decrypted_data = cipher_suite.decrypt(encrypted_data)
+        decrypted_file_path = encrypted_file_path[:-10]  # remove '.encrypted' suffix
+        with open(decrypted_file_path, 'wb') as f:
+            f.write(decrypted_data)
+        os.remove(encrypted_file_path)
+        print(f"Decrypted: {encrypted_file_path} to {decrypted_file_path}")
+    except FileNotFoundError:
+        print(f"File not found: {encrypted_file_path}")
+    except InvalidToken:
+        print(f"Invalid token for file: {encrypted_file_path}. Possible key mismatch or file corruption.")
+    except Exception as e:
+        print(f"Failed to decrypt {encrypted_file_path}: {e}")
