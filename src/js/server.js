16c7dd363665dfab2062b1d50914770aa9552292
L344S <145486211+L344S@users.noreply.github.com>
2024-07-12 19:36:36 +0200

    UP: Refactor server.js to improve code readability and remove unnecessary console logs (clean up)

--- src/js/server.js
+++ src/js/server.js
@@ -3,6 +3,8 @@
  * It uses Express.js to create a simple server that listens on port 3000.
  * A BIT SAD CUZ I WANTED TO AVOID A BACKEND BUT I GUESS I HAVE TO DO IT
  */
+
+// Imports
 const express = require("express");
 const cors = require("cors");
 const { exec } = require("child_process");
@@ -10,46 +12,41 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// Enable CORS
 app.use(cors());
 
+// Define routes, one for each Python script
 app.get("/encrypt", (req, res) => {
-  console.log("The request to /encrypt has been received");
   const scriptPath = path.join(__dirname, "../python/encrypt.py");
   exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing Python script: ${error}`);
       res.status(500).send(`Error executing Python script: ${error}`);
       return;
     }
     if (stderr) {
-      console.error(`Python stderr: ${stderr}`);
       res.status(500).send(`Python stderr: ${stderr}`);
       return;
     }
-    console.log(`Python stdout: ${stdout}`);
     res.send(`Python stdout: ${stdout}`);
   });
 });
 
 app.get("/decrypt", (req, res) => {
-  console.log("The request to /decrypt has been received");
   const scriptPath = path.join(__dirname, "../python/decrypt.py");
   exec(`python3 ${scriptPath}`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`Error executing Python script: ${error}`);
       res.status(500).send(`Error executing Python script: ${error}`);
       return;
     }
     if (stderr) {
-      console.error(`Python stderr: ${stderr}`);
       res.status(500).send(`Python stderr: ${stderr}`);
       return;
     }
-    console.log(`Python stdout: ${stdout}`);
     res.send(`Python stdout: ${stdout}`);
   });
 });
 
+// Start the server, listening on port 3000
 app.listen(port, "127.0.0.1", () => {
   console.log(`Server running at http://127.0.0.1:${port}`);
 });
